65256deb3720ac90053cbaa4acb4d0407dd03d92
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-07 19:04:55 -0500

    messing with rust

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod levenshtein;
 use levenshtein::*;
 
 fn main() {
-    let s1 = "add";
-    let s2 = "dady";
-    levenshtein(s1.to_string(), s2.to_string());
+    let s1 = "add".to_string();
+    let s2 = "dady".to_string();
+    levenshtein(s1, s2);
 }
